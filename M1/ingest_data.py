5c3a817c11694d5c97d84393a5cde24641593d5d
John Portin <jportin13@gmail.com>
2024-01-18 04:32:02 +0000

    create dockerfile

--- M1/ingest_data.py
+++ M1/ingest_data.py
@@ -14,40 +14,41 @@ def main(params):
     table_name = params.table_name
     url = params.url
 
+    print("type of port", type(port))
+
     chunk_size = 100000
 
     # download the Parquet file
     outputFile = "output.parquet"
     os.system(f"wget {url} -O {outputFile}")
 
-    engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{db}')
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     engine.connect()
     parquet_file = pq.ParquetFile(outputFile)
 
+    count = 0
     for batch in parquet_file.iter_batches(batch_size=chunk_size):
+        count += 1
+        print(f"Writing chunk {count} of size {len(batch)}")
         chunk_df = pa.Table.from_batches([batch]).to_pandas(split_blocks=True, self_destruct=True)
         chunk_df.to_sql(name="yellow_taxi_data", con=engine, if_exists="append")
 
 
-parser = argparse.ArgumentParser(description="Ingest Parquet data to Postgres")
 
-# user
-# password
-# host
-# port
-# database name
-# table name
-# url of the csv
+if __name__ == "__main__":
 
-parser.add_argument('user', help='user name for postgres')
-parser.add_argument('password', help='password for postgres')
-parser.add_argument('host', help='host for postgres')
-parser.add_argument('port', help='port for postgres')
-parser.add_argument('db', help='database name for postgres')
-parser.add_argument('table-name', help='name of the table where we will write the results to')
-parser.add_argument('url', help='url of the Parquet file')
+    parser = argparse.ArgumentParser(description="Ingest Parquet data to Postgres")
 
-args = parser.parse_args()
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the Parquet file')
 
+    args = parser.parse_args()
+
+    main(args)
 
 
